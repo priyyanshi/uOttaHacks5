1df5bfaddc5f3b2e19b7e0ed9a6e00c126bc2014
Cassey Shao <casseys0000@gmail.com>
2023-02-11 10:22:16 -0500

    Reference code for Chrome extension alert

--- popup.js
+++ popup.js
@@ -1,1 +1,12 @@
-console.log("hello")
+// console.log("hello")
+
+// Simple alert:
+// Reference: https://medium.com/tech-tajawal/build-a-simple-google-chrome-extension-in-few-minutes-1f13b600e83e
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('clickit');
+  checkPageButton.addEventListener('click', function() {
+
+    alert("Hello! This is a list of frontend bugs!")
+
+  }, false);
+}, false);
